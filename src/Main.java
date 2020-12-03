58fdce0f73e197789640ea252f1075ca20b6d7b5
Leoliu345 <1768117178@qq.com>
2020-12-03 16:50:12 +0800

    test

--- src/Main.java
+++ src/Main.java
@@ -11,7 +11,13 @@ public class Main {
     public static String terminals = new String(new char[]{'#', '+', '*', '(', ')', 'i'});
 
     //0=error,1=less,2=more,3=equal
-    public static int[][] matrix = {{3, 1, 1, 1, 1, 1}, {2, 2, 1, 1, 2, 1}, {2, 2, 2, 1, 2, 1}, {2, 1, 1, 1, 3, 1}, {2, 2, 2, 0, 2, 0}, {2, 2, 2, 0, 2, 0}};
+    public static int[][] matrix =
+                    {{3, 1, 1, 1, 1, 1},
+                    {2, 2, 1, 1, 2, 1},
+                    {2, 2, 2, 1, 2, 1}, 
+                    {2, 1, 1, 1, 3, 1},
+                    {2, 2, 2, 0, 2, 0},
+                    {2, 2, 2, 0, 2, 0}};
     public static Stack<Character> symbols = new Stack<>();
 
     public static void main(String[] args) {
@@ -21,37 +27,38 @@ public class Main {
             //BufferedReader in = new BufferedReader(new FileReader("D:\\学习\\大三上\\compile\\OPG\\src\\test.txt"));
             BufferedReader in = new BufferedReader(new FileReader(args[0]));
             while ((nextline = in.readLine()) != null) {
-                String fixLine = "#" + nextline + "#";
-                for (int i = 0; i < fixLine.length(); i++) {
-                    char next = fixLine.charAt(i);
-                    if (symbols.empty())
-                        symbols.push(next);
-                    else {
-                        char old = peekTerminal();
-                        int x = terminals.indexOf(old);
-                        int y = terminals.indexOf(next);
-                        if (x == -1 || y == -1) {
-                            System.out.println("E");
-                            return;
-                        }
-                        int judgePriority = matrix[x][y];
-                        if (judgePriority == 1) {
-                            symbols.push(next);
-                            System.out.println("I" + next);
-                        } else if (judgePriority == 2) {
-                            stipulation();
-                            i--;
-                            System.out.println("R");
-                        } else if (judgePriority == 3) {
-                            if (next == '#') return;
-                            symbols.push(next);
-                            System.out.println("I"+next);
-                        } else {
-                            System.out.println("E");
-                            return;
-                        }
-                    }
-                }
+                System.out.println(nextline);
+//                String fixLine = "#" + nextline + "#";
+//                for (int i = 0; i < fixLine.length(); i++) {
+//                    char next = fixLine.charAt(i);
+//                    if (symbols.empty())
+//                        symbols.push(next);
+//                    else {
+//                        char old = peekTerminal();
+//                        int x = terminals.indexOf(old);
+//                        int y = terminals.indexOf(next);
+//                        if (x == -1 || y == -1) {
+//                            System.out.println("E");
+//                            return;
+//                        }
+//                        int judgePriority = matrix[x][y];
+//                        if (judgePriority == 1) {
+//                            symbols.push(next);
+//                            System.out.println("I" + next);
+//                        } else if (judgePriority == 2) {
+//                            stipulation();
+//                            i--;
+//                            System.out.println("R");
+//                        } else if (judgePriority == 3) {
+//                            if (next == '#') return;
+//                            symbols.push(next);
+//                            System.out.println("I"+next);
+//                        } else {
+//                            System.out.println("E");
+//                            return;
+//                        }
+//                    }
+//                }
             }
         } catch (IOException e) {
             e.printStackTrace();
